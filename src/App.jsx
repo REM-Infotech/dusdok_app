11aa1706228ec0c8c233be21a17c02af05e48712
Robotz213 <nicholas@robotz.dev>
2025-05-07 17:53:37 -0300

    feat: Refactor project structure and update configurations, including new CSS files and logo assets

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,13 @@ export default {
     return (
       <>
         <header>
-          <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+          <img
+            alt="Vue logo"
+            class="logo"
+            src="../src/assets/svg/logo.svg"
+            width="125"
+            height="125"
+          />
 
           <div class="wrapper">
             <HelloWorld msg="You did it!" />
