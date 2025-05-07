5cb6f9c6d7e4583270952e376e8ed82430b2a7bd
Robotz213 <nicholas@robotz.dev>
2025-05-07 17:26:42 -0300

    feat: add Capacitor configuration and update dependencies

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.remdevs.dusdok',
+  appName: 'dusdok_app',
+  webDir: 'dist'
+};
+
+export default config;
