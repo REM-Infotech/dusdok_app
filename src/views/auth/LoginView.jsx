f360b42aa002f5943249c7a5f87d40e8247aee4f
Robotz213 <nicholas@robotz.dev>
2025-05-07 21:54:31 -0300

    reativo

--- src/views/auth/LoginView.jsx
+++ src/views/auth/LoginView.jsx
@@ -1,19 +1,96 @@
 import "@/assets/css/sign-in.css";
 import styled from "@vue-styled-components/core";
 import { BContainer, BFormGroup, BInput } from "bootstrap-vue-next";
-import { computed, onBeforeMount, ref } from "vue";
+import { onBeforeMount } from "vue";
 
-const floatingName = ref("");
+import { InputController } from "./inputHandle.ts";
 
-const floatingState = computed(() =>
-  floatingName.value.length >= 4 ? true : floatingName.value.length === 0 ? null : false,
-);
+/**
+ *  @type {import("./inputHandle.ts").InptController}
+ */
+const inputState = InputController();
+/**
+ *  @type {import("./inputHandle.ts").InptController}
+ */
+const inputStatePassword = InputController();
 
-const floatingInvalidFeedback = computed(() =>
-  floatingName.value.length > 1 ? "Enter at least 4 characters." : "Please enter something.",
-);
+inputStatePassword.invalidFeedback.value;
 
-const name = ref("");
+const Reactive = {
+  setup() {
+    onBeforeMount(() => {
+      document.body.classList.add(...["d-flex", "align-items-center", "py-4", "bg-purple"]);
+    });
+  },
+
+  render() {
+    return (
+      <>
+        <Container>
+          <form action="">
+            <img
+              class="mb-4"
+              src="./src/assets/brand/bootstrap-logo.svg"
+              alt=""
+              width="72"
+              height="57"
+            />
+            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+            <FloatingForm
+              id="fieldset-4"
+              label="Name"
+              label-for="input-floating-4"
+              valid-feedback="Thank you!"
+              invalid-feedback={inputState.invalidFeedback.value}
+              state={inputState.floatingState.value}
+              floating
+            >
+              <InputLogin
+                id="input-floating-4"
+                v-model={inputState.floatingVal.value}
+                state={inputState.floatingState.value}
+                placeholder="Enter your name please"
+              />
+            </FloatingForm>
+            <FloatingForm
+              id="fieldset-4"
+              label="Name"
+              label-for="input-floating-4"
+              valid-feedback="Thank you!"
+              invalid-feedback={inputStatePassword.invalidFeedback.value}
+              state={inputStatePassword.invalidFeedback.value}
+              floating
+            >
+              <InputPassword
+                id="input-floating-4"
+                v-model={inputStatePassword.floatingVal.value}
+                state={inputStatePassword.floatingState.value}
+                placeholder="Enter your name please"
+              />
+            </FloatingForm>
+            <FormCheck>
+              <input
+                class="form-check-input"
+                type="checkbox"
+                value="remember-me"
+                id="checkDefault"
+              />
+              <label class="ms-2 form-check-label" for="checkDefault">
+                Remember me
+              </label>
+            </FormCheck>
+            <button class="btn btn-primary w-100 py-2" type="submit">
+              Sign in
+            </button>
+            <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2025</p>
+          </form>
+        </Container>
+      </>
+    );
+  },
+};
+
+export default Reactive;
 
 const Container = styled(BContainer)`
   /* background-color: var(--bs-blue-chill); */
@@ -37,68 +114,15 @@ const FloatingForm = styled(BFormGroup)`
   z-index: 2;
 `;
 
-export default {
-  setup() {
-    onBeforeMount(() => {
-      document.body.classList.add(...["d-flex", "align-items-center", "py-4", "bg-purple"]);
-    });
-  },
+const my_3 = styled("div")`
+  margin-top: 1rem !important;
+  margin-bottom: 1rem !important;
+`;
 
-  render() {
-    return (
-      <>
-        <Container>
-          <form action="">
-            <FloatingForm
-              id="fieldset-4"
-              label="Name"
-              label-for="input-floating-4"
-              valid-feedback="Thank you!"
-              invalid-feedback={floatingInvalidFeedback.value}
-              state={floatingState.value}
-              floating
-            >
-              <InputLogin
-                id="input-floating-4"
-                v-model={floatingName.value}
-                state={floatingState.value}
-                placeholder="Enter your name please"
-              />
-            </FloatingForm>
-            <FloatingForm
-              id="fieldset-4"
-              label="Name"
-              label-for="input-floating-4"
-              valid-feedback="Thank you!"
-              invalid-feedback={floatingInvalidFeedback.value}
-              state={floatingState.value}
-              floating
-            >
-              <InputPassword
-                id="input-floating-4"
-                v-model={floatingName.value}
-                state={floatingState.value}
-                placeholder="Enter your name please"
-              />
-            </FloatingForm>
-            <div class="form-check text-start my-3">
-              <input
-                class="form-check-input"
-                type="checkbox"
-                value="remember-me"
-                id="checkDefault"
-              />
-              <label class="form-check-label" for="checkDefault">
-                Remember me
-              </label>
-            </div>
-            <button class="btn btn-primary w-100 py-2" type="submit">
-              Sign in
-            </button>
-            <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2025</p>
-          </form>
-        </Container>
-      </>
-    );
-  },
-};
+const text_start = styled(my_3)`
+  text-align: left !important;
+`;
+const FormCheck = styled(text_start)`
+  display: block;
+  min-height: 1.5rem;
+`;
---
+++ src/views/auth/inputHandle.ts
@@ -0,0 +1,37 @@
+import { computed, ref, type ComputedRef } from "vue";
+
+
+
+
+interface InptController {
+
+  typeInput: string
+  floatingState: ComputedRef<boolean | null>
+  invalidFeedback: ComputedRef<string>
+
+}
+
+export class InputController implements InptController {
+
+
+  typeInput: string;
+
+  constructor(typeInput: string) {
+
+    this.typeInput = typeInput
+
+  }
+
+  public floatingVal = ref("")
+  public floatingState = computed(() =>
+    this.floatingVal.value.length >= 4 ? true : this.floatingVal.value.length === 0 ? null : false,
+  );
+  public invalidFeedback = computed(() =>
+    this.floatingVal.value.length > 1 ? "Enter at least 4 characters." : "Please enter something.",
+  );
+}
+
+export type { InptController };
+
+
+
